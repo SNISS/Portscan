3c7c1966e43953cb92b058513f1c73ce6cb32b41
SNISS <82043842+SNISS@users.noreply.github.com>
2021-07-21 16:31:48 -0300

    Update portscan.py

--- portscan.py
+++ portscan.py
@@ -2,7 +2,7 @@ import socket
 
 ip = input("Enter target ip: ")
 
-for doors in range(1, 65535):
+door [80,21,22,444,20,8080,3636,]
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     if s.connect_ex((ip, doors)) == 0:
         print(doors, "Open")
